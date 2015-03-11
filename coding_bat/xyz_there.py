a1fc72b23104f2f7f7bf4b9f8d256f54817e18d9
Bob Ziroll <elderziroll@gmail.com>
2015-03-11 16:31:30 -0600

    completed xyz_there and sum13

--- coding_bat/xyz_there.py
+++ coding_bat/xyz_there.py
@@ -1,7 +1,8 @@
 """
 Challenge:
 
-Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a
+period (.). So "xxyz" counts but "x.xyz" does not.
 
 xyz_there('abcxyz') --> True
 xyz_there('abc.xyz') --> False
@@ -11,9 +12,23 @@ xyz_there('xyz.abc') --> True
 
 def xyz_there(str):
     if "xyz" in str:
-        for i in range(len(str)-1):
+        if str.startswith("xyz"):
+            return True
+        for i in range(len(str)):
             if str[i] != "." and str[i+1:i+4] == "xyz":
                 return True
+    return False
+
+
+
+
+
+# def xyz_there(str):
+#     if "xyz" in str:
+#         for i in range(len(str)):
+#             if str[i] != "." and str[i+1:i+4] == "xyz":
+#                 return True
+#     return False
 
 print xyz_there('abcxyz')  # True
 print xyz_there('abc.xyz')  # False
